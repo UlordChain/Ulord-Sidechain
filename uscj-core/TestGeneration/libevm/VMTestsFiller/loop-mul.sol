e31b1c1511f68072ca7e06df58307ab9d1e8a8e6
Kwuaint <34888408+kwuaint@users.noreply.github.com>
2018-05-13 20:10:03 +0800

    Update loop-mul.sol

---
+++ uscj-core/TestGeneration/libevm/VMTestsFiller/loop-mul.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.18;
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
