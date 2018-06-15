0f5e479b00905fd66a888c175622e3a2da5c86d8
tehsunnliu <tehsunnliu@gmail.com>
2018-06-15 18:02:22 +0800

    Update RegisterUTTransaction

--- RegisterUTTransaction.java
+++ RegisterUTTransaction.java
@@ -19,206 +19,42 @@
 package tools;
 
 import co.usc.config.BridgeConstants;
-import co.usc.config.UscSystemProperties;
+import co.usc.config.BridgeTestNetConstants;
 import co.usc.core.UscAddress;
-import co.usc.peg.*;
-import co.usc.ulordj.core.*;
-import co.usc.ulordj.store.BlockStoreException;
-import co.usc.ulordj.store.UldBlockStore;
-import org.ethereum.config.DefaultConfig;
-import org.ethereum.core.Repository;
-import org.ethereum.crypto.ECKey;
-import org.ethereum.vm.PrecompiledContracts;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
+import co.usc.peg.AddressBasedAuthorizer;
+import co.usc.ulordj.core.UldTransaction;
+import co.usc.ulordj.params.MainNetParams;
+import co.usc.ulordj.params.TestNet3Params;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
-/*
- * Set VM Option to
- * -Dusc.conf.file=<federation-config-file>.conf
- */
-
-@Component
 public class RegisterUTTransaction {
 
-    Repository repository;
-    UscSystemProperties config;
-    BridgeConstants bridgeConstants;
-    BridgeStorageProvider provider;
-
-    private enum StorageFederationReference { NONE, NEW, OLD, GENESIS }
-
-    @Autowired
-    RegisterUTTransaction(Repository repository,
-                          UscSystemProperties config) throws BlockStoreException {
-        this.repository = repository;
-        this.config = config;
-
-        this.bridgeConstants = this.config.getBlockchainConfig().getCommonConstants().getBridgeConstants();
-        provider = new BridgeStorageProvider(repository, new UscAddress(PrecompiledContracts.BRIDGE_ADDR_STR), config.getBlockchainConfig().getCommonConstants().getBridgeConstants());
-    }
-
     public static void main(String[]args){
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(DefaultConfig.class);
-        RegisterUTTransaction runner = ctx.getBean(RegisterUTTransaction.class);
-        runner.registerUTTransaction(args);
+        //registerUldTransaction <tx hex> <height> <merkletree>
+        registerUT(BridgeTestNetConstants.getInstance(), "674f05e1916abc32a38f40aa67ae6b503b565999", "abcd1234", "fe472c8a0ad06f7ba682fe5a60bf9245496928be8f1e18a47ce6d4682921a176");
     }
 
-    public void registerUTTransaction(String[]args){
-        Federation federation = getActiveFederation();
-        List<UldECKey> publicKeys = federation.getPublicKeys();
-        //TODO: Check if transaction is initiated for the multisig address on ulord.
-
+    public static boolean registerUT(BridgeConstants bridgeConstants, String fedAddress, String pwd, String utTxId) {
         try {
-            getUTBlocks(args);
-        }catch (IOException ex){
+            AddressBasedAuthorizer federationChangeAuthorizer = bridgeConstants.getFederationChangeAuthorizer();
 
-        }catch(Exception ex){
+            if (!federationChangeAuthorizer.isAuthorized(new UscAddress(fedAddress)))
+                return false;
 
+            // Try to unlock account
+            if (!Utils.tryUnlockUscAccount(fedAddress, pwd))
+                throw new PrivateKeyNotFoundException();
+
+            UldTransaction tx;
+            if(bridgeConstants instanceof BridgeTestNetConstants)
+                tx = Utils.getUldTransaction(TestNet3Params.get(), utTxId);
+            else
+                tx = Utils.getUldTransaction(MainNetParams.get(), utTxId);
+
+
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
         }
-        //String []args = new String[]{""};
-        //Encode_eth_call.getRegisterUldTransactionString(args);
-    }
-
-    public void getUTBlocks(String []args) throws IOException {
-        StringBuilder getBlockCount = new StringBuilder();
-        StringBuilder getBlockHash = new StringBuilder();
-        StringBuilder getBlock = new StringBuilder();
-
-        getBlockCount.append(NetworkConstants.ULORD_CLI);
-        getBlockHash.append(NetworkConstants.ULORD_CLI);
-        getBlock.append(NetworkConstants.ULORD_CLI);
-
-        if (args.length > 0 && args[0].equals("testnet")) {
-            getBlockCount.append(NetworkConstants.ULORD_TESTNET);
-            getBlockHash.append(NetworkConstants.ULORD_TESTNET);
-            getBlock.append(NetworkConstants.ULORD_TESTNET);
-        }
-
-        getBlockCount.append(" getblockcount");
-        getBlockHash.append(" getblockhash");
-        getBlock.append(" getblock");
-
-        Process proc = null;
-        Runtime rt = Runtime.getRuntime();
-
-        //Get the ulord best block height.
-        int bestBlockHeight = getBestBlockHeight(getBlockCount.toString());
-
-        //Get Block hash of the best block in ulord blockchain
-        String line = getBlockHashByHeight(getBlockHash.toString() + " " + bestBlockHeight);
-
-        //Get Block from ulord blockchain using the block hash.
-        String blockAsJsonString = getBlockAsJsonString(getBlock.toString() + " " + line);
-
-        JSONObject json = new JSONObject(blockAsJsonString);
-        JSONArray tx = json.getJSONArray("tx");
-        System.out.println(tx.get(0));
-        System.out.println(json.getString("previousblockhash"));
-    }
-
-    public int getBestBlockHeight(String getBlockCount) throws IOException{
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(getBlockCount);
-
-        InputStream inStr = proc.getInputStream();
-        InputStreamReader isr = new InputStreamReader(inStr);
-        BufferedReader br = new BufferedReader(isr);
-        return Integer.parseInt(br.readLine());
-    }
-
-    public String getBlockHashByHeight(String getBlockHash) throws IOException{
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(getBlockHash);
-        InputStream inStr = proc.getInputStream();
-        InputStreamReader isr = new InputStreamReader(inStr);
-        BufferedReader br = new BufferedReader(isr);
-        String line = br.readLine();
-        return line;
-    }
-
-    public String getBlockAsJsonString(String getBlock) throws IOException{
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(getBlock);
-        InputStream inStr = proc.getInputStream();
-        InputStreamReader isr = new InputStreamReader(inStr);
-        BufferedReader br = new BufferedReader(isr);
-        String line = br.readLine();
-        String blockAsJsonString = "";
-        while(line != null){
-            blockAsJsonString += line;
-            line = br.readLine();
-        }
-        return blockAsJsonString;
-    }
-
-    /**
-     * Returns the currently active federation.
-     * See getActiveFederationReference() for details.
-     * @return the currently active federation.
-     */
-    public Federation getActiveFederation() {
-        switch (getActiveFederationReference()) {
-            case NEW:
-                return provider.getNewFederation();
-            case OLD:
-                return provider.getOldFederation();
-            case GENESIS:
-            default:
-                return bridgeConstants.getGenesisFederation();
-        }
-    }
-
-    /**
-     * Returns the currently active federation reference.
-     * Logic is as follows:
-     * When no "new" federation is recorded in the blockchain, then return GENESIS
-     * When a "new" federation is present and no "old" federation is present, then return NEW
-     * When both "new" and "old" federations are present, then
-     * 1) If the "new" federation is at least bridgeConstants::getFederationActivationAge() blocks old,
-     * return the NEW
-     * 2) Otherwise, return OLD
-     * @return a reference to where the currently active federation is stored.
-     */
-    private StorageFederationReference getActiveFederationReference() {
-        Federation newFederation = provider.getNewFederation();
-
-        // No new federation in place, then the active federation
-        // is the genesis federation
-        if (newFederation == null) {
-            return StorageFederationReference.GENESIS;
-        }
-
-        Federation oldFederation = provider.getOldFederation();
-
-        // No old federation in place, then the active federation
-        // is the new federation
-        if (oldFederation == null) {
-            return StorageFederationReference.NEW;
-        }
-
-        // Both new and old federations in place
-        // If the minimum age has gone by for the new federation's
-        // activation, then that federation is the currently active.
-        // Otherwise, the old federation is still the currently active.
-        if (shouldFederationBeActive(newFederation)) {
-           return StorageFederationReference.NEW;
-        }
-
-        return StorageFederationReference.OLD;
-    }
-
-    private boolean shouldFederationBeActive(Federation federation) {
-        //long federationAge = uscExecutionBlock.getNumber() - federation.getCreationBlockNumber();
-        //return federationAge >= bridgeConstants.getFederationActivationAge();
-        return true;
     }
 }
