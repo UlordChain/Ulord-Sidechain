d13b0adff6dde64631a6560622892f108a97d934
Kwuaint <king_lear@tom.com>
2018-07-12 09:05:36 +0800

    comments for TestNet3Params

--- uscj-core/src/main/java/co/usc/mine/MinerClientImpl.java
+++ uscj-core/src/main/java/co/usc/mine/MinerClientImpl.java
@@ -143,7 +143,7 @@ public class MinerClientImpl implements MinerClient {
         work = minerServer.getWork();
 
         //get the ulord RegTest NetworkParameters
-        co.usc.ulordj.core.NetworkParameters ulordNetworkParameters = co.usc.ulordj.params.RegTestParams.get();
+        co.usc.ulordj.core.NetworkParameters ulordNetworkParameters = co.usc.ulordj.params.RegTestParams.get();//TestNet3Params for solo
         //get the CoinbaseTransaction of UMMB
         co.usc.ulordj.core.UldTransaction ulordMergedMiningCoinbaseTransaction = MinerUtils.getUlordMergedMiningCoinbaseTransaction(ulordNetworkParameters, work);
         //get the UMMB
