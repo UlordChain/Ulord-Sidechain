71c3254c59df506563412be1918de0894a140ba3
Kwuaint <king_lear@tom.com>
2018-07-25 16:14:56 +0800

    Add scoring.punishmentDisabled flag

--- uscj-core/src/test/java/co/usc/net/NodeMessageHandlerTest.java
+++ uscj-core/src/test/java/co/usc/net/NodeMessageHandlerTest.java
@@ -1,6 +1,6 @@
 /*
- * This file is part of RskJ
- * Copyright (C) 2017 RSK Labs Ltd.
+ * This file is part of USC
+ * Copyright (C) 2016 - 2018  Ulord Core team.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -970,7 +970,12 @@ public class NodeMessageHandlerTest {
     }
 
     private static PeerScoringManager createPeerScoringManager() {
-        return new PeerScoringManager(1000, new PunishmentParameters(600000, 10, 10000000), new PunishmentParameters(600000, 10, 10000000));
+        return new PeerScoringManager(
+                PeerScoring::new,
+                1000,
+                new PunishmentParameters(600000, 10, 10000000),
+                new PunishmentParameters(600000, 10, 10000000)
+        );
     }
 }
 
