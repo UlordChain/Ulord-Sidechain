fdb29daf31de5573311620c57a99caf4c5152294
tehsunnliu <tehsunnliu@gmail.com>
2018-08-07 09:31:37 +0800

    Added options to Whitelist

--- WhitelistUlordAddress.java
+++ WhitelistUlordAddress.java
@@ -19,7 +19,6 @@ import co.usc.ulordj.params.MainNetParams;
 import co.usc.ulordj.params.RegTestParams;
 import co.usc.ulordj.params.TestNet3Params;
 import com.typesafe.config.Config;
-import org.ethereum.util.RLPList;
 import org.ethereum.vm.PrecompiledContracts;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -35,18 +34,16 @@ public class WhitelistUlordAddress {
 
     private static Logger logger = LoggerFactory.getLogger("Federation");
     private static NetworkParameters params;
-    public static void main(String[] args) {
+    private static String lockWhitelistChangeAddress;
+    private static String lockWhitelistChangePassword;
+    private static BridgeConstants bridgeConstants;
 
-        if(args.length < 2) {
-            System.out.println("<Ulord address to whitelist> <Max value in Satoshi>");
-            return;
-        }
+    public static void main(String[] args) {
 
         FederationConfigLoader configLoader = new FederationConfigLoader();
         Config config = configLoader.getConfigFromFiles();
 
         String paramName = config.getString("blockchain.config.name");
-        BridgeConstants bridgeConstants;
         if(paramName.equals("testnet")) {
             bridgeConstants = BridgeTestNetConstants.getInstance();
             params = TestNet3Params.get();
@@ -58,14 +55,106 @@ public class WhitelistUlordAddress {
             params = MainNetParams.get();
         }
 
-        String lockWhitelistChangeAddress = config.getString("federation.lockWhitelistChangeAddress");
-        String lockWhitelistChangePassword = config.getString("federation.lockWhitelistChangePassword");
+        lockWhitelistChangeAddress = config.getString("federation.lockWhitelistChangeAddress");
+        lockWhitelistChangePassword = config.getString("federation.lockWhitelistChangePassword");
 
-        Address utAddress = new Address(params, args[0]);
+        if(args.length < 1) {
+            help();
+            return;
+        }
+
+        String response = "";
+        switch (args[0]) {
+            case "addWhitelistAddress":
+                if(args.length < 3) {
+                    System.out.println("addWhitelistAddress <UlordAddress> <Amount in Satoshi>");
+                    return;
+                }
+                response = addWhitelistAddress(args[1], args[2]);
+                break;
+            case "removeWhitelistAddress":
+                if(args.length < 2) {
+                    System.out.println("removeWhitelistAddress <UlordAddress>");
+                    return;
+                }
+                response = removeWhitelistAddress(args[1]);
+                break;
+            case "listWhitelistAddress":
+                response = listWhitelistAddress();
+                break;
+            case "setWhitelistDisableBlockDelay":
+                if(args.length < 2) {
+                    System.out.println("setWhitelistDisableBlockDelay <nBlocks>");
+                    return;
+                }
+                response = setWhitelistDisableBlocksDelay(args[1]);
+                break;
+            default:
+                help();
+                break;
+        }
+
+        if(response.isEmpty()) {
+            response = null;
+        }
+        System.out.println(response);
+    }
+
+    private static String setWhitelistDisableBlocksDelay(String nBlock) {
+        try {
+            if(sendTx(lockWhitelistChangeAddress, DataEncoder.encodeSetLockWhitelistDisableBlockDelay(new BigInteger(nBlock)), 3))
+                return "Whitelist disable blocks delay successfully set";
+            else
+                return "Whitelist disable blocks delay failed";
+
+        } catch (Exception e) {
+            return  "Exception in setWhitelistDisableBlocksDelay " + e;
+        }
+    }
+
+    private static String listWhitelistAddress() {
+        try {
+            String callResponse = UscRpc.call(PrecompiledContracts.BRIDGE_ADDR_STR, Hex.toHexString(Bridge.GET_LOCK_WHITELIST_SIZE.encodeSignature()));
+            JSONObject callResponseJson = new JSONObject(callResponse);
+            long whitelistSize = Long.parseLong(callResponseJson.get("result").toString().substring(2), 16);
+
+            String addresses = "";
+            for (int i = 0; i < whitelistSize; i++) {
+                callResponse = UscRpc.call(PrecompiledContracts.BRIDGE_ADDR_STR, Hex.toHexString(Bridge.GET_LOCK_WHITELIST_ADDRESS.encode(new Object[]{i})));
+                callResponseJson = new JSONObject(callResponse);
+                String result = callResponseJson.get("result").toString().substring(2);
+                Object[] objects = Bridge.GET_LOCK_WHITELIST_ADDRESS.decodeResult(Hex.decode(result));
+
+                addresses += objects[0].toString() + "\n";
+            }
+            return addresses;
+        } catch (Exception ex) {
+            return "Exception ins listWhitelistAddress" + ex;
+        }
+    }
+
+    private static String removeWhitelistAddress(String addressToRemove) {
+        try {
+            // Try to unlock account
+            if(!Utils.tryUnlockUscAccount(lockWhitelistChangeAddress, lockWhitelistChangePassword))
+                throw new PrivateKeyNotFoundException();
+
+            if(sendTx(lockWhitelistChangeAddress, DataEncoder.encodeRemoveLockWhitelistAddress(addressToRemove), 3)) {
+                return "Successfully to removed " +  addressToRemove +" from whitelist";
+            } else {
+                return "Failed to remove " +  addressToRemove +" from whitelist";
+            }
+        } catch (Exception e) {
+            return "Exception in removeWhitelistAddress: " + e;
+        }
+    }
+
+    private static String addWhitelistAddress(String addressToWhitelist, String amountInSatoshi) {
+        Address utAddress = new Address(params, addressToWhitelist);
         UscAddress whitelistAuthorisedAddress = new UscAddress(lockWhitelistChangeAddress);
 
         boolean isWhitelisted = false;
-        if(whitelistAddress(bridgeConstants, whitelistAuthorisedAddress, lockWhitelistChangePassword, utAddress, Coin.valueOf(Long.valueOf(args[1])))) {
+        if(whitelistAddress(bridgeConstants, whitelistAuthorisedAddress, lockWhitelistChangePassword, utAddress, Coin.valueOf(Long.valueOf(amountInSatoshi)))) {
             // Confirm if the whitelist address is stored in the blockchain
             try {
                 String callResponse = UscRpc.call(PrecompiledContracts.BRIDGE_ADDR_STR, Hex.toHexString(Bridge.GET_LOCK_WHITELIST_SIZE.encodeSignature()));
@@ -81,20 +170,30 @@ public class WhitelistUlordAddress {
                     String address = objects[0].toString();
                     if(utAddress.toString().equals(address)) {
                         isWhitelisted = true;
-                        //break;
+                        break;
                     }
-                    // Print all the whitelisted addresses
-                    System.out.println(address);
                 }
             } catch (Exception e) {
-                System.out.println(e);
+                return "Exception in addWhitelistAddress: " + e;
             }
         }
 
         if (isWhitelisted)
-            System.out.println("Successfully whitelisted");
+            return "Successfully whitelisted";
         else
-            System.out.println("Whitelist failed");
+            return "Whitelist failed";
+    }
+
+    private static void help() {
+        System.out.println("<function name> <param1> <param2> ...");
+        System.out.println("Whitelist Management:" + "\n"
+                + "------------------------------------------------------------------------------------------------------------------------------------" + "\n"
+                + "   Functions:                            Parameters" + "\n"
+                + "addWhitelistAddress                      <UlordAddress> <Amount in Satoshi>" + "\n"
+                + "removeWhitelistAddress                   <UlordAddress>" + "\n"
+                + "setWhitelistDisableBlockDelay            <nBlocks>" + "\n"
+                + "------------------------------------------------------------------------------------------------------------------------------------" + "\n"
+        );
     }
 
     public static boolean whitelistAddress(BridgeConstants bridgeConstants, UscAddress whitelistAuthorisedAddress, String pwd, Address utAddress, Coin valueInSatoshi) {
