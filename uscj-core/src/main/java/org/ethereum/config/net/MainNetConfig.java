02f3156d381b191a0133ab2eb612037824631979
Teh Sunn Liu (AlexLiu) <tehsunnliu@gmail.com>
2018-11-07 13:52:31 +0800

    Update UnlimitedWhitelist fork time

--- uscj-core/src/main/java/org/ethereum/config/net/MainNetConfig.java
+++ uscj-core/src/main/java/org/ethereum/config/net/MainNetConfig.java
@@ -30,7 +30,7 @@ import org.ethereum.config.blockchain.MainNetUnlimitedWhitelistConfig;
 public class MainNetConfig extends AbstractNetConfig {
     public MainNetConfig() {
         add(0, new MainNetBeforeBridgeSyncConfig());
-        add(144550, new MainNetUnlimitedWhitelistConfig());
+        add(144000, new MainNetUnlimitedWhitelistConfig());
         // 60 days of 1 block every 14 seconds.
         // On blockchain launch blocks will be faster until difficulty is adjusted to available hashing power.
         add(370000, new MainNetAfterBridgeSyncConfig());
