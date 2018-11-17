0fdaa36ea85e19383124dda4aa809cdc66883dee
Alex Liu <tehsunnliu@gmail.com>
2018-11-17 15:40:18 +0800

    Updated configure.sh and removed RSK Dependencies

--- configure.sh
+++ configure.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-GRADLE_WRAPPER="0f49043be582d7a39b671f924c66bd9337b92fa88ff5951225acc60560053067"
-DOWNLOADED_HASH2=''
-DOWNLOAD_FILE2=$(dd if=/dev/urandom bs=64 count=1 2>/dev/null| od -t x8 -A none  | tr -d ' '\\n)
 unamestr=`uname`
 
 
@@ -11,23 +8,18 @@ function downloadJar(){
 	if [ ! -d ./uscj-core/libs ]; then
 		mkdir ./uscj-core/libs
 	fi
-	curl https://deps.rsklabs.io/gradle-wrapper.jar -o ~/$DOWNLOAD_FILE2
-    if [[ $PLATFORM == 'linux' || $PLATFORM == 'windows' ]]; then
-        DOWNLOADED_HASH2=$(sha256sum ~/${DOWNLOAD_FILE2} | cut -d' ' -f1)
-    elif [[ $PLATFORM == 'mac' ]]; then
-        DOWNLOADED_HASH2=$(shasum -a 256 ~/${DOWNLOAD_FILE2} | cut -d' ' -f1)
-    fi
-
-    if [[ $GRADLE_WRAPPER != $DOWNLOADED_HASH2 ]]; then
-        rm -f ~/${DOWNLOAD_FILE2}
-        exit 1
-    else
-        mv ~/${DOWNLOAD_FILE2} ./gradle/wrapper/gradle-wrapper.jar
-        rm -f ~/${DOWNLOAD_FILE2}
-    fi
 
 	curl -o ./uscj-core/libs/ulordj-thin-0.0.1-usc-1-bundled.jar -L https://github.com/UlordChain/ulordj-thin/releases/download/ulordj-thin-0.0.1/ulordj-thin-0.0.1-usc-1-bundled.jar;
-    curl -o ./uscj-core/libs/libCryptoHello.so -L https://github.com/UlordChain/ulordj-thin/releases/download/ulordj-thin-0.0.1/libCryptoHello.so
+    curl -o ./uscj-core/libs/lll-compiler-0.0.2.jar -L https://github.com/UlordChain/lll-compiler/releases/download/0.0.2/lll-compiler-0.0.2.jar
+    curl -o ./uscj-core/libs/bclcrypto-jdk15on-1.59.jar -L https://github.com/UlordChain/lll-compiler/releases/download/0.0.2/bclcrypto-jdk15on-1.59.jar
+
+    if [[ $PLATFORM == 'linux'  ]]; then
+        sudo curl -o /usr/lib/libCryptoHello.so -L https://github.com/UlordChain/ulordj-thin/releases/download/ulordj-thin-0.0.1/libCryptoHello.so
+    elif [[ $PLATFORM == 'windows' ]]; then
+        echo "Platform currently not supported"
+    elif [[ $PLATFORM == 'mac' ]]; then
+        echo "Platform currently not supported"
+    fi
 }
 
 function platform() {
