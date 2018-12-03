772a01c9aa9cfa398453f93a572c52b429dad11b
zhangwanbing1 <zhangwanbing@live.com>
2018-12-03 16:33:24 +0800

    Add comment for the UscIPs

--- uscj-core/src/main/java/org/ethereum/config/blockchain/mainnet/MainNetAfterBridgeSyncConfig.java
+++ uscj-core/src/main/java/org/ethereum/config/blockchain/mainnet/MainNetAfterBridgeSyncConfig.java
@@ -63,52 +63,62 @@ public class MainNetAfterBridgeSyncConfig extends GenesisConfig {
     }
 
 
-    @Override   // UscIP97
+    @Override   // UscIP97 Remove difficulty drop
     public BlockDifficulty calcDifficulty(BlockHeader curBlock, BlockHeader parent) {
         return getBlockDifficulty(getConstants(), curBlock, parent);
     }
 
+    // Whitelisting adds unlimited option
     @Override
     public boolean isUscIP87() {return true;}
 
+    // Improvements to REMASC contract
     @Override
     public boolean isUscIP85() {
         return true;
     }
 
+    // Bridge local calls
     @Override
     public boolean isUscIP88() { return true; }
 
+    // Improve blockchain block locator
     @Override
     public boolean isUscIP89() {
         return true;
     }
 
+    // Add support for return EXTCODESIZE for precompiled contracts
     @Override
     public boolean isUscIP90() {
         return true;
     }
 
+    // Add support for STATIC_CALL opcode
     @Override
     public boolean isUscIP91() {
         return true;
     }
 
+    // Storage improvements
     @Override
     public boolean isUscIP92() {
         return true;
     }
 
+    // Code Refactor, removes the sample contract
     @Override
     public boolean isUscIP93() {
         return true;
     }
 
+    // Disable OP_CODEREPLACE
     @Override
     public boolean isUscIP94() {
         return true;
     }
 
+    // Disable fallback mining in advance
     @Override
     public boolean isUscIP98() {
         return true;
